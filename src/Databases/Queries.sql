302cc5a50c1343605426c9c4e274847acd5bd91f
Jonathan Henrique Silva <poliekos@outlook.com>
2024-05-25 00:17:03 -0300

    Querie para criar tabela

--- src/Databases/Queries.sql
+++ src/Databases/Queries.sql
@@ -1,6 +1,14 @@
 -- Como estou usando Entity Framework, faremos consultas T-SQL equivalentes aos metodos do CRUD.
 
-
+-- Criar a tabela Usuarios
+CREATE TABLE Usuarios (
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Nome NVARCHAR(100) NOT NULL,
+	Email NVARCHAR(100) NOT NULL,
+	Senha NVARCHAR(100) NOT NULL,
+	CPF NVARCHAR(11) NOT NULL,
+	DataNasc DATE NOT NULL
+);
 -- Insercao
 INSERT INTO Usuarios (Nome, Email, Senha, CPF, DataNasc) VALUES ('Joao', 'joao@gmail.com', '123456', '12345678901', '1990-01-01');
 -- Melhor fazer a conversao de data no client-side, para evitar problemas de timezone.
